2b399e829f4807a76c0c80fe32a3690ba5226814
EIA485 <17285570+EIA485@users.noreply.github.com>
2022-08-11 13:44:08 -0700

    proj setup

---
+++ OpenGlovesHaptics/EyeFaceAPI.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+
+namespace OpenGlovesHaptics
+{
+    public class OpenGlovesHaptics : NeosMod
+	{
+		public override string Name => "OpenGlovesHaptics";
+		public override string Author => "dfgHiatus, eia485";
+		public override string Version => "1.0.0";
+		public override string Link => "https://github.com/dfgHiatus/NeosOpenGlovesHaptics/";
+		public override void OnEngineInit()
+		{
+			new Harmony("net.dfgHiatus.OpenGlovesHaptics").PatchAll();
+        }
+	}
+}
